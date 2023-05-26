83e9e01bb126a982a5becbcf891ef5603e1469c9
Sourabh Mane <msourabh631@gmail.com>
2023-05-26 19:08:59 +0530

    Create README.md

--- src/main/java/com/geekster/Ecommerce_API/Models/User.java
+++ src/main/java/com/geekster/Ecommerce_API/Models/User.java
@@ -1,10 +1,10 @@
 package com.geekster.Ecommerce_API.Models;
 
-
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
